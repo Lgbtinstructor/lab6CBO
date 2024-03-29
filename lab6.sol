e93db15cb30a204150602882fda5a0127725c9e6
Lgbtinstructor <134735915+Lgbtinstructor@users.noreply.github.com>
2024-03-29 22:58:46 +0500

    Update lab6.sol

--- lab6.sol
+++ lab6.sol
@@ -1,19 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-
 contract CBO {
     string public name;
     string public symbol;
     uint8 public decimals;
     uint256 public totalSupply;
-    
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
-    
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
     event Mint(address indexed to, uint256 value);
-    
     constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _initialSupply) {
         name = _name;
         symbol = _symbol;
@@ -21,23 +17,19 @@ contract CBO {
         totalSupply = _initialSupply * 10 ** uint256(_decimals);
         balanceOf[msg.sender] = totalSupply;
     }
-    
     function transfer(address _to, uint256 _value) public returns (bool success) {
         require(_to != address(0), "Invalid address");
         require(balanceOf[msg.sender] >= _value, "Insufficient balance");
-        
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
         emit Transfer(msg.sender, _to, _value);
         return true;
     }
-    
     function approve(address _spender, uint256 _value) public returns (bool success) {
         allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
-    
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         require(_to != address(0), "Invalid address");
         require(balanceOf[_from] >= _value, "Insufficient balance");
@@ -49,7 +41,6 @@ contract CBO {
         emit Transfer(_from, _to, _value);
         return true;
     }
-    
     function mint(address _to, uint256 _value) public returns (bool success) {
     require(_to != address(0), "Invalid address");
     totalSupply += _value * (10 ** uint256(decimals));
@@ -57,5 +48,4 @@ contract CBO {
     emit Mint(_to, _value * (10 ** uint256(decimals)));
     return true;
 }
-
 }
